5479af8628c216ee9eaf5c82d8660e9cf4834c4d
Mike Conrad <mike@enxo.co>
2020-03-11 19:19:53 -0400

    Updated version info

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,8 +1,5 @@
 
     var disableButton = document.getElementById("disableButton");
-    var br1 = document.getElementById("br1");
-    var br2 = document.getElementById("br2");
-    var disableButton = document.getElementById("disableButton");
 
     chrome.storage.sync.get('kitty', function(data){
       console.log(data.kitty)
@@ -22,10 +19,10 @@ disableButton.addEventListener('click', function(){
 
   chrome.storage.sync.get('kitty', function(data) {
 
-      var enabled = (data.kitty) ? !data.kitty : 'false'
+      var enabled = !data.kitty
       chrome.storage.sync.set({'kitty': enabled})
     
-    disableButton.innerText = (enabled) ? 'Disable' : 'Enable'
+    disableButton.innerText = (enabled === true) ? 'Disable' : 'Enable'
   });
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
